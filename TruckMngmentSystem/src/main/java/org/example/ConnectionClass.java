8dcacf168ec5926001cb84ce4f77d30a0e87e7c2
Mukul_Saini <118501986+8813801277@users.noreply.github.com>
2024-10-06 05:08:31 +0530

    Update ConnectionClass.java

--- TruckMngmentSystem/src/main/java/org/example/ConnectionClass.java
+++ TruckMngmentSystem/src/main/java/org/example/ConnectionClass.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class ConnectionClass {
     //public static final String Load_driver = "com.mysql.cj.jdbc.Driver";
-    public static final String URL = "jdbc:mysql://localhost:3306/truckmgt";
-    public static final String Password = "Papa@1234";
-    public static final String Username = "root";
+    public static final String URL = "enter your db url here";
+    public static final String Password = "enter your password of db";
+    public static final String Username = "enter your username of db";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL  , Username , Password);
